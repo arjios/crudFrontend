6407579f6b6ab05463d1d88fcce282d3e44ed493
Arjios <antonio.uab.ios.fisica@gmail.com>
2020-01-15 14:57:34 -0300

    Criado repositorio

---
+++ src/components/template/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Logo.css'
+
+import logo from '../../assets/imgs/logo.png'
+
+export default props => 
+    <aside ClassName='logo'>
+        <a href="/" className='logo'>
+            <img src={logo} alt='logo' />
+        </a>
+    </aside>
---
+++ src/main/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+
+import Main from '../components/template/Main'
+import Nav from '../components/template/Nav'
+import Logo from '../components/template/Logo'
+import Footer from '../components/template/Footer'
+
+export default props =>
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main />
+        <Footer />
+    </div>
